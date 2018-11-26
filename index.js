282d47528f3bd56b975f95e8eb3450929d14b0ff
Stublink <chrisrey95@gmail.com>
2018-11-26 13:59:09 -0500

    :zap: hashed password before inserting into db + ;ogin endpoint to compare hashed passwords

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcryptjs');
 
 const db = require('./database/dbConfig.js');
 
@@ -12,10 +13,41 @@ server.get('/', (req, res) => {
   res.send('Its Alive!');
 });
 
+server.post('/api/login', (req, res) => {
+  // grab username and password
+  const creds = req.body;
+
+  db('users').where({username: creds.username}).first()
+  .then(user => {
+    if(user && bcrypt.compareSync(creds.password, user.password)) {
+      res.status(201).json({message: 'can longin'})
+    } else {
+      res.status(401).json({message: 'you shall not pass!'})
+    }
+  })
+  .catch(err => console.log(err));
+})
+
+server.post('/api/register', (req, res) => {
+  // grab username and password
+  const creds = req.body;
+
+  // generate the hash from users password
+  const hash = bcrypt.hashSync(creds.password, 14) // rounds 2 ^ X
+
+  // override the user.password with hashed password
+  creds.password = hash
+
+  // save user to db
+  db('users').insert(creds)
+  .then(ids => res.status(201).json(ids))
+  .catch(err => console.log(err))
+})
+
 // protect this route, only authenticated users should see it
 server.get('/api/users', (req, res) => {
   db('users')
-    .select('id', 'username')
+    .select('id', 'username', 'password') // added password to the select
     .then(users => {
       res.json(users);
     })
